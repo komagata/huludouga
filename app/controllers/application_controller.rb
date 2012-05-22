aa71da1fec8797d91584797f1e954744753fcf4f
Masaki Komagata <komagata@gmail.com>
2012-05-22 20:11:19 +0900

    Fix header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_access
 
-  def set_access
-    @response.headers["Access-Control-Allow-Origin"] = "*"
+  before_filter :allow_cross_domain_access
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
   end
 end
