2fb527331c26960eda3a0065fcfc57ec1712d512
Masaki Komagata <komagata@gmail.com>
2012-05-22 19:42:14 +0900

    Add comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   attr_accessible :id, :body, :movie_id, :position, :updated_at, :created_at
+  validates :body,     presence: true
+  validates :movie_id, presence: true
+  validates :position, presence: true
 end
