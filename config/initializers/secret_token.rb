1b9ceb987c4440df06bf8ce335998bbc7dd0ecd2
Masaki Komagata <komagata@gmail.com>
2012-05-22 18:56:23 +0900

    First add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huludouga::Application.config.secret_token = '7ea4eeddaa417fea085430eab7de0e901f72eaaf1b6b6a712b24c4f227e5056e41be11404588037af7abb9e6519ecb05e07ead5611c43f99a214063408d1263c'
