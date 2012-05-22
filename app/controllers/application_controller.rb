9d49cfeea504f0ee6d4bdeb933efb618a2246f29
Masaki Komagata <komagata@gmail.com>
2012-05-22 20:09:33 +0900

    Add Access-Control-Allow-Origin header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_access
+
+  def set_access
+    @response.headers["Access-Control-Allow-Origin"] = "*"
+  end
 end
